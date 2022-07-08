b33b585d3d1935f64bd04b3541136bd6f1cef616
Ricciardo Simone <ricciardo855@gmail@com>
2022-07-08 10:36:46 +0200

    fix numeri fino a 6 + inserito pareggio

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -2,16 +2,19 @@
 let MainButton = document.getElementById('main-button');
 MainButton.addEventListener('click', function(){
 
-    const numeroComputer = Math.floor(Math.random() * 5 + 1);
+    const numeroComputer = Math.floor(Math.random() * 6 + 1);
     document.getElementById("numeroPC").innerHTML = numeroComputer;
 
-    const numeroGiocatore = Math.floor(Math.random() * 5 + 1);
+    const numeroGiocatore = Math.floor(Math.random() * 6 + 1);
     document.getElementById("numeroGiocatore").innerHTML = numeroGiocatore;
 
     if(numeroGiocatore > numeroComputer)
       document.getElementById("risultato").innerHTML = "hai vinto";
     else
       document.getElementById("risultato").innerHTML = "hai perso";
-  
+
+    if(numeroGiocatore == numeroComputer)  
+      document.getElementById("risultato").innerHTML = "pareggio";
+
 });
   
